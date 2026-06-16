9f89314a1cc505e43af409735079d7ded0155516
2521002647-svg <2521002647@sv.ufm.edu.vn>
2026-06-16 14:01:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Tiêu đề ứng dụng
-st.title("💰 Ứng dụng tính tiền gửi tiết kiệm")
+st.title("APP TÍNH TIỀN GỬI TIẾT KIỆM TẠI NGÂN HÀNG_ĐỀ TÀI 4_NGUYỄN GIA HUY")
 
 # Nhập dữ liệu
 C = st.number_input(
